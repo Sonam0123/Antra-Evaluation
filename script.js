0e227b3f739854b7cf89fd63a78dc118dee5ed7e
sonam <tseringsonam34@hotmail.com>
2022-08-01 14:23:00 -0400

    getting data from JSON

--- script.js
+++ script.js
@@ -2,18 +2,16 @@ const button = document.querySelector('.submit');
 const input = document.querySelector('.todo-input');
 const main = document.querySelector('.main');
 
-button.addEventListener('click', function(e) {
-  e.preventDefault();
-  const todo = input.value;
-  const li = document.createElement('p');
-  li.innerText = todo;
-  input.classList.add('todo');
-  li.classList.add('todo-item');
-  main.appendChild(li);
-  input.value = '';
-  li.addEventListener('click', function(e) {
-    li.style.textDecoration = "line-through";
-    // e.target.remove();
+
+
+axios({
+    method: 'GET',
+    url: ('http://localhost:3000/todos')
     })
-});
-  
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
